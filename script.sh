969669aaeadaff2b23f6f3a4a5fe236ecb8dc195
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2025-12-01 00:31:55 +0100

    initial commit

---
+++ script.sh
@@ -0,0 +1,3 @@
+# replace the content with "copy as value -> copy as CURL" from the network monitor
+content=<redacted>
+echo "$content">actual.in
---
+++ template.cpp
@@ -0,0 +1,50 @@
+#include <cassert>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <vector>
+
+template <typename T>
+auto split(std::string_view str, char delim) {
+    std::vector<T> result;
+    for (const auto &&word : std::views::split(str, delim)) {
+        try {
+            auto str = std::string(std::begin(word), std::end(word));
+            if constexpr (std::is_integral<T>::value)
+                result.push_back(stoll(str));
+            else if constexpr (std::is_floating_point<T>::value)
+                result.push_back(stold(str));
+            else if constexpr (std::is_same_v<T, std::string>)
+                result.emplace_back(str);
+            else if constexpr (std::is_same_v<T, std::string_view>)
+                result.emplace_back(str);
+            else
+                static_assert(false, "split<T>: wyd blud");
+        } catch (...) {
+            assert(false && "wyd blud");
+        }
+    }
+    return result;
+}
+
+template <typename T>
+auto solve(T vec) -> void {
+    (void) vec;
+}
+
+auto main() -> int {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+
+    constexpr bool SINGLE = true;  // update this!
+
+    std::vector<std::string> lines;
+    std::string line;
+    while (getline(std::cin, line))
+        lines.push_back(std::move(line));
+
+    if constexpr (SINGLE)
+        solve(lines);
+    else
+        for (auto &line : lines)
+            solve(line);
+}
